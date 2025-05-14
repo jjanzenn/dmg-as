8420abdc93f16720f766104f8f179585465c89b5
jjanzen <me@jjanzen.ca>
2025-05-14 13:24:38 -0500

    read tokens

--- test/token.c
+++ test/token.c
@@ -18,11 +18,68 @@ void setUp(void)
 
 void tearDown(void) { fclose(f); };
 
-void test_thing(void) { TEST_ASSERT_EQUAL_INT(4, 4); }
+void test_get_each_token(void)
+{
+    char **tokens = malloc(sizeof(char *) * 40);
+    {
+        tokens[0] = "_main";
+        tokens[1] = ":";
+        tokens[2] = "nop";
+        tokens[3] = "\n";
+        tokens[4] = "ld";
+        tokens[5] = "b";
+        tokens[6] = ",";
+        tokens[7] = "c";
+        tokens[8] = "\n";
+        tokens[9] = "\n";
+        tokens[10] = "label1";
+        tokens[11] = ":";
+        tokens[12] = "\n";
+        tokens[13] = "pop";
+        tokens[14] = "bc";
+        tokens[15] = "\n";
+        tokens[16] = "_vblank";
+        tokens[17] = ":";
+        tokens[18] = "ld";
+        tokens[19] = "b";
+        tokens[20] = ",";
+        tokens[21] = "label1";
+        tokens[22] = "\n";
+        tokens[23] = "\n";
+        tokens[24] = "nop";
+        tokens[25] = "\n";
+        tokens[26] = "\n";
+        tokens[27] = "label2";
+        tokens[28] = ":";
+        tokens[29] = "nop";
+        tokens[30] = "\n";
+        tokens[31] = "ld";
+        tokens[32] = "b";
+        tokens[33] = ",";
+        tokens[34] = "label2";
+        tokens[35] = "\n";
+        tokens[36] = "label3";
+        tokens[37] = ":";
+        tokens[38] = "nop";
+        tokens[39] = "\n";
+    };
+
+    int i = 0;
+    char *tok = NULL;
+    do {
+        tok = next_token(f);
+        if (tok) {
+            TEST_ASSERT_LESS_THAN(40, i);
+            TEST_ASSERT_EQUAL_STRING(tokens[i++], tok);
+        }
+    } while (tok != NULL);
+    TEST_ASSERT_EQUAL_INT(40, i);
+    free(tokens);
+}
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_thing);
+    RUN_TEST(test_get_each_token);
     return UNITY_END();
 }
--- token.c
+++ token.c
@@ -1,4 +1,55 @@
 #include "token.h"
+#include <ctype.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-char *next_token(FILE *f) { return NULL; }
+#define DEFAULT_STR_SIZE 16
+
+struct token_string {
+    size_t capacity;
+    size_t size;
+    char *data;
+};
+
+static void string_append(struct token_string *str, char c)
+{
+    if (str->size == str->capacity) {
+        str->capacity *= 2;
+        str->data = realloc(str->data, str->capacity);
+    }
+    str->data[str->size++] = c;
+}
+
+char *next_token(FILE *f)
+{
+    if (!f)
+        return NULL;
+
+    struct token_string str = {
+        .capacity = DEFAULT_STR_SIZE,
+        .size = 0,
+        .data = malloc(DEFAULT_STR_SIZE),
+    };
+
+    int c = fgetc(f);
+    if (c == ':' || c == ',' || c == '\n') {
+        string_append(&str, c);
+    } else if (c == EOF) {
+        free(str.data);
+        return NULL;
+    } else {
+        while ((c == ' ' || c == '\t' || c == '\r' || c == '\f') && c != EOF) {
+            c = fgetc(f);
+        }
+        while (c != EOF && c != ':' && c != ',' && c != '\n' && c != ' ' &&
+               c != '\t' && c != '\r' && c != '\f') {
+            string_append(&str, tolower(c));
+            c = fgetc(f);
+        }
+        ungetc(c, f);
+    }
+
+    string_append(&str, 0);
+    return str.data;
+}
