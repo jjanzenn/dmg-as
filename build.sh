970ba0ee7cfded2ba14820a13602f03db32cb027
jjanzen <me@jjanzen.ca>
2025-05-28 11:51:41 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$(uname -s)" = 'Darwin' ]; then
+    if [ "$(uname -p)" = 'arm' ]; then
+        echo Apple Silicon detected...
+        export CMAKE_INCLUDE_PATH=/opt/homebrew/opt/flex/include
+        export CMAKE_LIBRARY_PATH=/opt/homebrew/opt/flex/lib
+        export PATH="/opt/homebrew/opt/flex/bin:/opt/homebrew/opt/bison/bin:$PATH"
+    else
+        echo x86_64 detected...
+        export CMAKE_INCLUDE_PATH=/usr/local/opt/flex/include
+        export CMAKE_LIBRARY_PATH=/usr/local/opt/flex/lib
+        export PATH="/usr/local/opt/flex/bin:/usr/local/opt/bison/bin:$PATH"
+    fi
+fi
+
+mkdir -p build/
+cd build/ || exit
+cmake ..
+make
